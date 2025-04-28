facf029a64b1564f6575c6fb463cef6ec9323877
RO-HIT17 <rohit.selvakumaran@gmail.com>
2025-04-29 01:08:41 +0530

    add todo

--- todo.c
+++ todo.c
@@ -1,13 +1,24 @@
-// user/todo.c
-
 #include "types.h"
 #include "stat.h"
 #include "user.h"
 
-// declare system calls
+// Declare system calls
 int todo_add(char *desc);
 int todo_list(void);
 int todo_done(int id);
+
+// Custom strcat implementation
+void strcat(char *dest, const char *src) {
+    while (*dest) {  
+        dest++;
+    }
+    while (*src) {  
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    *dest = '\0';  
+}
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -16,11 +27,22 @@ int main(int argc, char *argv[]) {
     }
 
     if (strcmp(argv[1], "add") == 0) {
-        if (argc != 3) {
-            printf(2, "Usage: todo add \"task description\"\n");
+        if (argc < 3) {
+            printf(2, "Usage: todo add <description>\n");
             exit();
         }
-        todo_add(argv[2]);
+
+        char desc[512];
+        desc[0] = '\0';  // Start with empty string
+
+        for (int i = 2; i < argc; i++) {
+            strcat(desc, argv[i]);
+            if (i != argc - 1) {
+                strcat(desc, " ");  // Add space between words
+            }
+        }
+
+        todo_add(desc);
     } else if (strcmp(argv[1], "list") == 0) {
         todo_list();
     } else if (strcmp(argv[1], "done") == 0) {
